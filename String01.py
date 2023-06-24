bde49d77d7f78b36a4cb8684ca3521e6725a3df9
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-24 14:51:52 +0500

    1-masala yechimi

--- String01.py
+++ String01.py
@@ -5,4 +5,13 @@ def main():
     Returns:
         str: return answer.
     """
-    return 
+    s = ""
+    if len(a)==8:
+        s=a
+    return s
+a = "salomber"
+print(main())
+a = "qalesan"
+print(main())
+a = "PythonUz"
+print(main())
